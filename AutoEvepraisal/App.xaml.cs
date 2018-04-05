e4ca8556bdf3d03b0f2aa25e653636faa722d1d5
Trevor Elliott <trevorlloydelliott@gmail.com>
2018-04-05 02:32:36 -0400

    Add solution

---
+++ AutoEvepraisal/App.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace AutoEvepraisal
+{
+    public partial class App : Application
+    {
+        private const string ItemLineRegexFormat = "^[0-9]+ [A-Za-z0-9\\- ]+$";
+
+        private HttpClient _httpClient;
+        private string _lastClipboardContents;
+
+        public App()
+        {
+            _httpClient = new HttpClient();
+            _httpClient.BaseAddress = new Uri("https://evepraisal.com");
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var timer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromMilliseconds(100)
+            };
+
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (Clipboard.ContainsText())
+            {
+                var clipboardText = Clipboard.GetText();
+                if (_lastClipboardContents != clipboardText)
+                {
+                    _lastClipboardContents = clipboardText;
+
+                    var match = Regex.Match(clipboardText, ItemLineRegexFormat, RegexOptions.Multiline);
+                    if (match.Success)
+                    {
+                        var response = _httpClient.PostAsync($"appraisal.json?market=jita&raw_textarea={Uri.EscapeUriString(clipboardText)}&persist=no", new StringContent(String.Empty)).Result;
+
+                        if (response.IsSuccessStatusCode)
+                        {
+                            var popup = new Popup();
+                            popup.Text = response.Content.ReadAsStringAsync().Result;
+                            popup.Show();
+
+                            var hideTimer = new DispatcherTimer
+                            {
+                                Interval = TimeSpan.FromSeconds(4),
+                            };
+                            hideTimer.Tick += (s, _) =>
+                            {
+                                popup.Close();
+                            };
+                            hideTimer.Start();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ AutoEvepraisal/Popup.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AutoEvepraisal
+{
+    public partial class Popup : Window
+    {
+        public string Text
+        {
+            get
+            {
+                return textBox.Text;
+            }
+            set
+            {
+                textBox.Text = value;
+            }
+        }
+
+        public Popup()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
